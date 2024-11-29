b64d2c0ab3bbb4f02082678c4925ef17d878325a
diyorkhon <diyorbekulashev580@gmail.com>
2024-11-29 11:48:16 +0500

    postga author qushmoqchiman

--- homepage/models.py
+++ homepage/models.py
@@ -3,3 +3,14 @@ from termios import TIOCSER_TEMT
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=200)
+    text=models.TextField()
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete= models.CASCADE,
+        default=1
+    )
+
+    def __str__(self):
+        return self.title
--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView , ListView
+from .models import Post
 # Create your views here.
-class Homepageview(TemplateView):
+class Homepageview(ListView):
+     model=Post
      template_name = "home.html"
+
+
 class AboutPageViews(TemplateView):
      template_name = "about.html"
